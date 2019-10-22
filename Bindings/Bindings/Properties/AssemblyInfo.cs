6fe47b3101540f5d7c87f8875d7637bda95e7a88
Edilon Andrade <edilonandrade@gmail.com>
2019-10-22 10:08:24 -0300

    Incluindo as primeiras modificações de Bindings

---
+++ Bindings/Bindings/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bindings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bindings")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f421218-3809-4ac6-ac5d-662006ddd4ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bindings/Bindings/Steps/ExampleHookExecutionOrderSteps.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace Bindings.Steps
+{
+    [Binding]
+    public class ExampleHookExecutionOrderSteps
+    {
+        [BeforeScenario(Order = 100)]
+        public void LoginUser()
+        {
+            Debug.Print("...so we can log in to a clean \n");
+        }
+
+        [BeforeScenario(Order = 0)]
+        public void CleanDatabase()
+        {
+            Debug.Print("we need to run this first\n");
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Debug.Print("Não implementado, somente para teste do BeforeScenario Order\n");
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Debug.Print("Não implementado, somente para teste do BeforeScenario Order\n");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Debug.Print("Não implementado, somente para teste do BeforeScenario Order\n");
+        }
+
+    }
+}
