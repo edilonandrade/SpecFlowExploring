fcaccfe32d9dc551876539804925295ae2ce9520
Edilon Andrade <edilonandrade@gmail.com>
2019-12-26 11:11:15 -0300

    Exemplos do pedidos em estoque com Given recebendo string no parâmetro.

---
+++ StepArgumentTransformations/StepArgumentTransformations/Features/OrderingItemStock.feature
@@ -0,0 +1,25 @@
+﻿Feature: OrderingItemStock
+	
+Scenario: Ordering an item in stock
+  Given we have '3' items in stock.
+  When we order '1' item
+  Then we should have '2' items in stock left.
+
+Scenario: Ordering the last item in stock
+  Given we have '1' items in stock.
+  When we order '1' item
+  Then we should have '0' items in stock left.
+
+Scenario: Ordering no item in stock
+  Given we have 'no' items in stock.
+  When we order '1' item
+  Then we should have '-1' items in stock left.
+
+Scenario Outline: Ordering items in stock
+  Given we have '<ItemsInStock>' items in stock.
+  When we order '<OrderedItems>' item
+  Then we should have '<ItemsLeft>' items in stock left.
+Examples:
+| ItemsInStock | OrderedItems | ItemsLeft |
+|            3 |            1 |         2 |
+|            1 |            1 |         0 |
---
+++ StepArgumentTransformations/StepArgumentTransformations/Steps/OrderingItemStockSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace StepArgumentTransformations.Steps
+{
+    [Binding]
+    public class OrderingItemStockSteps
+    {
+        [Given(@"we have '(.*)' items in stock\.")]
+        public void GivenWeHaveItemsInStock(string p0)
+        {
+            Debug.Print(ScenarioContext.Current.ScenarioInfo.Title);
+            Debug.Print($"Parâmetro {p0}");
+        }
+        
+        [When(@"we order '(.*)' item")]
+        public void WhenWeOrderItem(int p0)
+        {
+            Debug.Print(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+        
+        [Then(@"we should have '(.*)' items in stock left\.")]
+        public void ThenWeShouldHaveItemsInStockLeft_(int p0)
+        {
+            Debug.Print(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+    }
+}
