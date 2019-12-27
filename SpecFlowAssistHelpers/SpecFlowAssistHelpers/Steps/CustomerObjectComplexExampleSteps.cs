5a37b522e98edd40052a0631ec65e28b4a099cc1
Edilon Andrade <edilonandrade@gmail.com>
2019-12-27 18:53:23 -0300

    Exemplo de objeto complexo na tabela. Para este uso é necessário criar uma extension.

---
+++ SpecFlowAssistHelpers/SpecFlowAssistHelpers/Steps/CustomerObjectComplexExampleSteps.cs
@@ -0,0 +1,17 @@
+﻿using SpecFlowAssistHelpers.Extensions;
+using SpecFlowAssistHelpers.Models;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowAssistHelpers.Steps
+{
+    [Binding]
+    public class CustomerObjectComplexExampleSteps
+    {
+        [Given(@"I have the following customers")]
+        public void GivenIHaveTheFollowingCustomers(Table customer)
+        {
+            var _customer = customer.VerifyPropertiesAndCreateSet<Customer>();
+        }
+    }
+}
