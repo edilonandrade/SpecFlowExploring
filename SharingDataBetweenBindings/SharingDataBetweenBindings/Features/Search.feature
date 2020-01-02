29308760d6b2d59cb0369c2015d1f07e22cd8975
Edilon Andrade <edilonandrade@gmail.com>
2020-01-02 11:39:26 -0300

    Exemplo incompleto do compartilhamento e dados.

---
+++ SharingDataBetweenBindings/SharingDataBetweenBindings/Features/Search.feature
@@ -0,0 +1,5 @@
+﻿Feature: Search
+
+Scenario: Busca	
+	When I perform a simple search on 'livro'
+	Then the book list should exactly contain book 'livro'
---
+++ SharingDataBetweenBindings/SharingDataBetweenBindings/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharingDataBetweenBindings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharingDataBetweenBindings")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f47b7d5-f4c1-4bb2-91b8-cd05c1f111e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
